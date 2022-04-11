1df82247428838309e829e0c5efa5d186c337a6f
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-10 20:55:03 -0700

    added projectInstall, project contributions, tests, and licensing

--- index.js
+++ index.js
@@ -24,7 +24,40 @@ const promptUser = () => {
                 type: "Sounds great! Please offer a brief description of your project. What does it do? What is the goal? What tools were/are used?",
                 name: "description",
             },
-            
+            {
+                type: "list",
+                message: "Do you want a license on this project?",
+                name: "license",
+                choices: ["MIT", "Gpl-3.0", "Unlicense", "Apache-2.0", "None", "Maybe later"],
+            },
+            {
+                type: "input",
+                message: "What are your rules on usage of this project?",
+                name: "usage",
+            },
+            {
+                type: "input",
+                message: "Do you want to add instructions on how to install and use your project?",
+                name: "projectInstall"
+            },
+            {
+                type: "input",
+                message: "How can users contribute to this project?",
+                name: "contributions",
+            },
+            {
+                type: "input",
+                message: "How can users test this project?",
+                name: "projectTests",
+            },
         ])
-    }
+        .then(function (answers) {
+            return answers;
+        });
+
+    };
+
+  
+
+
 }
